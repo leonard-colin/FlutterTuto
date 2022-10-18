82cf556a979b9b068fbbd9ab2820f66032291568
leonardtkt <leonard@tkt.paris>
2022-10-18 14:28:24 +0200

    begin camera screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:camera/camera.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:leadeetuto/screens/camera.dart';
 import 'package:leadeetuto/screens/dashboard/home.dart';
 import 'package:leadeetuto/screens/guest.dart';
 import 'package:leadeetuto/screens/services/user_service.dart';
@@ -9,39 +11,50 @@ import 'package:leadeetuto/screens/services/user_service.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  final List<CameraDescription> cameras = await availableCameras();
+
   await Firebase.initializeApp();
 
-  runApp(App());
+  runApp(App(
+    cameras: cameras,
+  ));
 }
 
 class App extends StatelessWidget {
   UserService _userService = UserService();
+  final List<CameraDescription> cameras;
+
+  App({super.key, required this.cameras});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Leadee',
-      home: StreamBuilder(
-        stream: _userService.user,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.active) {
-            if (snapshot.hasData) {
-              return HomeScreen();
-            }
-
-            return GuestScreen();
-          }
-
-          return SafeArea(
-            child: Scaffold(
-              body: Center(
-                child: Text("Loading..."),
-              ),
-            ),
-          );
-        },
+      home: CameraScreen(
+        cameras: cameras,
       ),
+
+      // StreamBuilder(
+      //   stream: _userService.user,
+      //   builder: (context, snapshot) {
+      //     if (snapshot.connectionState == ConnectionState.active) {
+      //       if (snapshot.hasData) {
+      //         return HomeScreen();
+      //       }
+
+      //       return GuestScreen();
+      //     }
+
+      //     return SafeArea(
+      //       child: Scaffold(
+      //         body: Center(
+      //           child: Text("Loading..."),
+      //         ),
+      //       ),
+      //     );
+      //   },
+      // ),
     );
   }
 }
---
+++ lib/screens/camera.dart
@@ -0,0 +1,145 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  const CameraScreen({
+    super.key,
+    required this.cameras,
+  });
+
+  @override
+  State<CameraScreen> createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  Future<void>? _initializeControllerFuture;
+  CameraController? _controller;
+
+  Future<void> initCamera(CameraDescription camera) async {
+    _controller = CameraController(
+      camera,
+      ResolutionPreset.medium,
+    );
+
+    _controller?.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+    });
+
+    if (_controller!.value.hasError) {
+      print("Camera Error ${_controller!.value.errorDescription}");
+    }
+
+    _initializeControllerFuture = _controller!.initialize();
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Future<void> _cameraToggle() async {
+    await initCamera(widget.cameras[0]);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _cameraToggle();
+  }
+
+  @override
+  void dispose() {
+    _controller!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Padding(
+          padding: const EdgeInsets.only(
+            top: 15,
+          ),
+          child: FutureBuilder(
+            future: _initializeControllerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Stack(
+                  children: [
+                    CameraPreview(_controller!),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        margin: EdgeInsets.only(
+                          bottom: 35.0,
+                        ),
+                        height: 60.0,
+                        width: 60.0,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            width: 3.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        child: FittedBox(
+                          child: InkWell(
+                            onLongPress: (() => print("take video")),
+                            child: FloatingActionButton(
+                              onPressed: () => print("take photo"),
+                              backgroundColor: Colors.transparent,
+                              elevation: 0.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Container(
+                        margin: EdgeInsets.only(
+                          bottom: 40,
+                          left: 60,
+                        ),
+                        height: 50.0,
+                        width: 50.0,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            width: 3.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        child: Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            customBorder: CircleBorder(),
+                            onTap: () => print("gallery access"),
+                            child: Icon(
+                              Icons.photo_size_select_actual,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                );
+              }
+              return Center(
+                child: Text("Loading"),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
